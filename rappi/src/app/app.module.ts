59efda0bc7172776f3ff6f326c976a335f81bde1
juan david   torres muñoz <juanda@MacBook-juanda.local>
2018-06-13 20:56:18 -0500

    arreglo

--- rappi/src/app/app.module.ts
+++ rappi/src/app/app.module.ts
@@ -2,21 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { MenuComponent } from './menu/menu.component';
-import { SliderComponent } from './slider/slider.component';
-import { ProductoComponent } from './producto/producto.component';
-import { CanastaComponent } from './canasta/canasta.component';
+import { MenuComponent } from './global/menu/menu.component';
+import { HeaderComponent } from './global/header/header.component';
+import { CanastaComponent } from './global/canasta/canasta.component';
+import { HomeComponent } from './pages/home/home.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     MenuComponent,
-    SliderComponent,
-    ProductoComponent,
+    HeaderComponent,
     CanastaComponent,
+    HomeComponent,
+  
     
   ],
   imports: [
